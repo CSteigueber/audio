c198c33ded9c2c5c792e83d7dbde7c922284fa8d
Claas Steigüber <56023114+CSteigueber@users.noreply.github.com>
2019-12-03 09:15:06 +0100

    old changes

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # audio
-
-Modified from https://gist.github.com/steventhebrave/7c16a72fb940b05b5e5218390418b5bf
-Thanks to Steven the brave for his example of the basic principle!
-
+html copied from https://gist.github.com/steventhebrave/7c16a72fb940b05b5e5218390418b5bf
--- script.js
+++ script.js
@@ -10,9 +10,8 @@ var years=[2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006
 years=years.reverse();
 var pop=[7713468100,7631091040,7547858925,7464022049,7379797139,7295290765,7210581976,7125828059,7041194301,6956823603,6872767093,6789088686,6705946610,6623517833,6541907027,6461159389,6381185114,6301773188,6222626606,6143493823,6064239055,5984793942,5905045788,5824891951,5744212979,5663150427,5581597546,5498919809,5414289444,5327231061,5237441558,5145426008,5052522147,4960567912,4870921740,4784011621,4699569304,4617386542,4536996762,4458003514,4380506100,4304533501,4229506060,4154666864,4079480606,4003794172,3927780238,3851650245,3775759617,3700437046,3625680627,3551599127,3478769962,3407922630,3339583597,3273978338,3211001009,3150420795,3091843507,3034949748,2979576185,2925686705,2873306090,2822443282,2773019936,2724846741,2677608960,2630861562,2584034261,2000000000,1600000000,1200000000,1000000000,770000000,610000000,500000000,450000000,350000000,360000000,320000000,275000000,240000000,220000000,210000000,200000000,190000000,150000000,100000000,50000000,27000000,14000000,7000000,5000000]
 pop=pop.reverse();
-console.log(years);
-console.log(pop);
-var year=1000;
+var year=2000;                      //Indicate Year to begin with here!
+var nPop=pop[years.indexOf(year)]; // setting nPop to the value cooresponding to the beginning year
 oscillatorNode.connect(gainNode);
 gainNode.connect(audioCtx.destination)
 oscillatorNode.start();
@@ -20,23 +19,40 @@ oscillatorNode.frequency.value = frequency;
 gainNode.gain.value = 0.0; //volume!
 document.getElementById("poprun").addEventListener("click",function (){
     gainNode.gain.value = 0.5; //volume!
+    var delta=0;
     (function manualLoop() {
         setTimeout(function() {
-            manualLoop();         
+            manualLoop();
             // restructure to enable gradient increase depending on next step!
             let i=years.indexOf(year);
-            if (i!=-1){
-                frequency=numToFreq(pop[i]);
-                oscillatorNode.frequency.value = frequency;       
-                document.getElementById('frequency').innerHTML = frequency.toFixed(2);
-                document.getElementById("pop").innerHTML=`world population: ${pop[i]/1000000} Mio`;
+            if ((i!=-1)&&(year<2019)){
+                let lastYear=years[i];
+                let nextYear=years[i+1];
+                let lastPop=pop[i];
+                let nextPop=pop[i+1];
+                if (nextYear==NaN){
+                    console.log("Oh No!");
+                    delta=0;
+                }
+                else{
+                    console.log("Yeah!");
+                    delta= (nextPop-lastPop)/(nextYear-lastYear);
+                }
+                    
             }
+            nPop+=delta;
+            document.getElementById("pop").innerHTML=`world population: ${(nPop/1000000).toFixed(2)} Mio`;
             document.getElementById("year").innerHTML=`Year: ${year}`;
+            frequency=numToFreq(nPop);
+            oscillatorNode.frequency.value = frequency;
+            document.getElementById('frequency').innerHTML = frequency.toFixed(2);
             if (year<2019){
                 year++;
             }
             else {
                 gainNode.gain.value=0;
+                delta=0;
+                nPop=pop[-1];
             }
         }, 50)
     }());
